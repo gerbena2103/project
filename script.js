e4dae28e0ed27e3be6254fd7509b1e1df1da8fbe
Illia <example@gmail.com>
2025-01-02 23:19:11 +0200

    first code in script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const isChecked = true,
-      isClose = false;
+"use strict"
 
-console.log(isChecked || isClose);git
+const numberOfFilms = +prompt("Сколько фильмов уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренних фильмов?", ""),
+      b = +prompt("На сколько оцените фильм?", ""),
+      c = prompt("Один из последних просмотренних фильмов?", ""),
+      d = +prompt("На сколько оцените фильм?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
